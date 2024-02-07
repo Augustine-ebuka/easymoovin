3c605ce0e7b8417db2fc37ffc954fcfffb307117
Obetta Augustine <hebrusgraphics@gmail.com>
2024-02-07 13:23:03 -0800

    connection to db

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('conneted to db');
+}).catch((error)=>{
+    console.log("error connecting", error);
+})
 
 
 const app = express()
